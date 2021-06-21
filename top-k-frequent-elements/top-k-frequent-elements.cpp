f5c55cb7aebfe2b523f9ae5c6c7faa498bf3f691
Karan Singh <44117714+ikaran-singh@users.noreply.github.com>
2021-06-21 13:33:11 +0530

    Time: 12 ms (92.46%), Space: 13.6 MB (70.96%) - LeetHub

--- top-k-frequent-elements/top-k-frequent-elements.cpp
+++ top-k-frequent-elements/top-k-frequent-elements.cpp
@@ -1,24 +1,21 @@
 class Solution {
 public:
-    
-    struct myCmp{
-        bool operator()(pair<int,int> p1, pair<int,int> p2){
-            if(p1.second==p2.second)
-                return p1.first>p2.first;
-            return p1.second<p2.second;
-        }
-    };
-    
     vector<int> topKFrequent(vector<int>& nums, int k) {
         
         vector<int> v;
-        unordered_map<int, int> m;
-        int n=nums.size();
-        for(int i=0;i<n;i++) m[nums[i]]++;
-        priority_queue<pair<int,int>,vector<pair<int,int>>,myCmp> pq(m.begin(),m.end());
+        unordered_map<int,int> mp;
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
         
-        for(int i=0;i<k;i++){
-            v.push_back(pq.top().first);
+        for(int i=0;i<nums.size();i++) mp[nums[i]]++;
+        
+        for(auto i=mp.begin();i!=mp.end();i++){
+            pq.push({i->second,i->first});
+            if(pq.size()>k)
+                pq.pop();
+        }
+        
+        while(pq.size()>0){
+            v.push_back(pq.top().second);
             pq.pop();
         }
         
