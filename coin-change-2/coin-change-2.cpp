f2411d38bc6ec9b836117cfb9fcb83152d08f7e6
Karan Singh <44117714+ikaran-singh@users.noreply.github.com>
2021-06-29 19:46:05 +0530

    Time: 20 ms (53.21%), Space: 11.9 MB (51.36%) - LeetHub

--- coin-change-2/coin-change-2.cpp
+++ coin-change-2/coin-change-2.cpp
@@ -1,28 +1,26 @@
 class Solution {
 public:
-    
-    int calculate(vector<int> coins, int n, int sum){
-        
-        int dp[sum+1][n+1];
-        for(int i=0;i<=n;i++)
-            dp[0][i]=1;
-        for(int i=1;i<=sum;i++)
-            dp[i][0]=0;
-        for(int i=1;i<=sum;i++){
-            for(int j=1;j<=n;j++){
-                dp[i][j]=dp[i][j-1];
-                if(coins[j-1]<=i)
-                    dp[i][j]+=dp[i-coins[j-1]][j];
-            }
-        }
-        return dp[sum][n];
-        
-    }
-    
     int change(int amount, vector<int>& coins) {
         
         int n=coins.size();
-        return calculate(coins,n,amount);
+        int t[n+1][amount+1];
         
+        for(int i=0;i<n+1;i++){
+            for(int j=0;j<amount+1;j++){
+                if(i==0) t[i][j]=0;
+                if(j==0) t[i][j]=1;
+            }
+        }
+        
+        for(int i=1;i<n+1;i++){
+            for(int j=1;j<amount+1;j++){
+                if(coins[i-1]<=j)
+                    t[i][j]=t[i-1][j]+t[i][j-coins[i-1]];
+                else
+                    t[i][j]=t[i-1][j];
+            }
+        }
+        
+        return t[n][amount];
     }
 };
