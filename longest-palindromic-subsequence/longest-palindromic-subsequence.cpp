e0c008c7ddb5d3377cfb1f745b20e1e0dcab79eb
Karan Singh <44117714+ikaran-singh@users.noreply.github.com>
2021-06-30 20:36:39 +0530

    Time: 64 ms (70.56%), Space: 10.8 MB (65.94%) - LeetHub

--- longest-palindromic-subsequence/longest-palindromic-subsequence.cpp
+++ longest-palindromic-subsequence/longest-palindromic-subsequence.cpp
@@ -1,31 +1,31 @@
 class Solution {
 public:
     
-    int longestCommonSubsequence(string s1, string s2) {
+    int lcs(string a, string b, int m, int n){
         
-        int m=s1.length(),n=s2.length();
-        int dp[m+1][n+1];
-        
-        for(int i=0;i<=m;i++) dp[i][0]=0;
-        for(int j=0;j<=n;j++) dp[0][j]=0;
-        
-        for(int i=1;i<=m;i++){
-            for(int j=1;j<=n;j++){
-                if(s1[i-1]==s2[j-1])
-                    dp[i][j]=1+dp[i-1][j-1];
-                else
-                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
-            }
-        }
-        return dp[m][n];
+        int t[m+1][n+1];
+	    for(int i=0;i<m+1;i++){
+	        for(int j=0;j<n+1;j++){
+	            if(i==0 || j==0) t[i][j]=0;
+	        }
+	    }
+	    
+	    for(int i=1;i<m+1;i++){
+	        for(int j=1;j<n+1;j++){
+	            if(a[i-1]==b[j-1])
+	                t[i][j]=1+t[i-1][j-1];
+	            else
+	                t[i][j]=max(t[i-1][j],t[i][j-1]);
+	        }
+	    }
+	    return t[m][n];        
     }
     
     int longestPalindromeSubseq(string s) {
         
-        string t=s;
-        reverse(t.begin(),t.end());
-        
-        return longestCommonSubsequence(s,t);
-        
+        int m=s.size();
+        string nw=s;
+        reverse(nw.begin(),nw.end());
+        return lcs(s,nw,m,m);
     }
 };
